2c6f3256701c194e963577edfe179d14f26625d4
huynhngocthang <huynhngocthang789@gmail.com>
2020-05-07 14:04:01 +0700

    .

---
+++ baitap.js
@@ -0,0 +1,18 @@
+let i=10,f=20.5,b=true,s="hà nội";
+document.write(i) 
+document.write("<br>")
+document.write(f)
+document.write("<br>")
+document.write(b)
+document.write("<br>")
+document.write(s)
+document.write("<br>")
+width=10,height=20;
+let area= width*height;
+document.write("area="+area);
+document.write("<br>");
+a=prompt,b=prompt;
+a=+prompt('nhập số thứ nhất');
+b=+prompt('nhập số thứ hai');
+ isdiv = a%b === 0;
+document.write(isdiv == true ? a/b : 'false');
